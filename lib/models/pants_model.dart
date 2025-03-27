b639c5c0e2be3317bd2f113f289f04d373f14cfd
Jakub Zawada <JakubZawadaa3@gmail.com>
2025-03-27 20:32:35 +0100

    Add PantsModel

---
+++ lib/models/pants_model.dart
@@ -0,0 +1,15 @@
+class PantsModel {
+  final String id;
+  final String name;
+  final double price;
+  final String image;
+  final List<String> sizes;
+
+  PantsModel({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.image,
+    required this.sizes,
+  });
+}
