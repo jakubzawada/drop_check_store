749fbd1db8b29a2a164b3514055df7e1cdb01323
Jakub Zawada <JakubZawadaa3@gmail.com>
2025-03-27 20:38:46 +0100

    Add BestsellerModel

---
+++ lib/models/bestseller_model.dart
@@ -0,0 +1,13 @@
+class BestsellerModel {
+  final String id;
+  final String name;
+  final double price;
+  final String image;
+
+  BestsellerModel({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.image,
+  });
+}
