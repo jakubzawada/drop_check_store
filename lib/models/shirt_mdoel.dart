c1eb180a9a493e0e0f1e8733a839d65994e8afc5
Jakub Zawada <JakubZawadaa3@gmail.com>
2025-03-27 20:35:11 +0100

    Add ShirtMdoel

---
+++ lib/models/shirt_mdoel.dart
@@ -0,0 +1,15 @@
+class ShirtMdoel {
+  final String id;
+  final String name;
+  final double price;
+  final String image;
+  final List<String> sizes;
+
+  ShirtMdoel({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.image,
+    required this.sizes,
+  });
+}
